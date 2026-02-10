46fad4d4e16e80f1a1f90a2f9f21ae88fc08b5bd
Jlassi-Oussema <Oussema.Jlassi@esprit.tn>
2026-02-10 18:01:06 +0100

    Workshop 3 : routage et lazy loading des suggestions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { HeaderComponent } from "./core/header/header.component";
+import { FooterComponent } from "./core/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ListSuggestionComponent, RouterOutlet],
+  imports: [ListSuggestionComponent, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // route par défaut
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  // pages principales
+  { path: 'home', component: HomeComponent },
+  { path: 'listSuggestion', component: ListSuggestionComponent },
+
+  // page 404
+  { path: '**', component: NotfoundComponent }
+];
